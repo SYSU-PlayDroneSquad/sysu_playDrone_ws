b4d50dcc253c2d682c02552c531f816311097178
Jet-Pan <3028481711@qq.com>
2022-03-17 15:15:26 +0800

    结项

--- setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-catkin_make -DCATKIN_WHITELIST_PACKAGES="dji_sdk"
-catkin_make -DCATKIN_WHITELIST_PACKAGES="hector_uav_msgs"
-catkin_make -DCATKIN_WHITELIST_PACKAGES=""
-echo "source ~/sysu_playDrone_ws/devel/setup.bash" >> ~/.bashrc
-source ~/.bashrc
